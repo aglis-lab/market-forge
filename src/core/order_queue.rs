cc8bbaa3f72761906312e18e30d109f10c415136
zaen <gooner0709@gmail.com>
2026-06-03 00:39:41 +0700

    feat: add comment to remembering if something off

--- src/core/order_queue.rs
+++ src/core/order_queue.rs
@@ -68,6 +68,8 @@ impl OrderQueue {
         Some(self.queue.remove(index).unwrap())
     }
 
+    // TODO: We can optimize it by using prev and next indices inside order allocator, but for simplicity, we will use binary search here.
+    // With those optimizations, we can achieve O(1) complexity for finding and removing orders, but it will require more complex data structures and management.
     #[inline(always)]
     pub fn find_order(&self, order_id: order::OrderId) -> Option<(usize, OrderMeta)> {
         return self
